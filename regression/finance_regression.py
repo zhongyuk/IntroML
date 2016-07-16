a196ed3e07cdf3b5a67a407c1f7210d33482314d
Zhongyu <dream.kuang@gmail.com>
2016-07-16 18:39:38 -0400

    finished regression mini proj

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,12 +38,13 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print reg.coef_
+print reg.intercept_
+print reg.score(feature_train, target_train)
+print reg.score(feature_test, target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +66,9 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print reg.coef_
+plt.plot(feature_train, reg.predict(feature_train), color='b')
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
