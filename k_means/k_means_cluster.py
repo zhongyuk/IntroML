6e3f49265f8d4512489d66897828b3aba993a554
Zhongyu <dream.kuang@gmail.com>
2016-07-16 20:20:32 -0400

    finished k_means mini proj

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,10 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
+#features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -64,8 +66,10 @@ plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+clt = KMeans(n_clusters=2)
+clt.fit(finance_features, poi)
+pred = clt.predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
